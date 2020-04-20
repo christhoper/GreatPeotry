e55f420a5cbba1a9a2856628000efd96c542b15c
hende <2419036884@qq.com>
2020-04-20 15:28:05 +0800

    创建项目

---
+++ GreatPeotry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreatPeotry
+//
+//  Created by bailun on 2020/4/17.
+//  Copyright © 2020 hend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+#关闭CocoaPods中的警告
+inhibit_all_warnings!
+
+target 'GreatPeotry' do
+
+   pod 'RxSwift'
+   pod 'SnapKit'
+   pod 'RxCocoa'
+
+end
