1dd8c023af91d1b3675a6b7108962b675772df86
hende <2419036884@qq.com>
2020-04-24 16:12:35 +0800

    完成初始模块的创建

---
+++ Home/Home/MainPage/MainPageProtocols.swift
@@ -0,0 +1,35 @@
+//
+//  MainPageMainPageProtocols.swift
+//  BL_VIPer.xcodeproj
+//
+//  Created by jhd on 24/04/2020.
+//  Copyright © 2020 BaiLun. All rights reserved.
+//
+
+// MARK: - ModuleProtocol
+
+/**
+ *  methods for communication OuterSide -> MainPage
+ *  define the capabilities of MainPage
+ */
+protocol MainPageModuleInput: class {}
+
+/**
+ *  methods for communication MainPage -> OuterSide
+ *  tell the caller what is changed
+ */
+protocol MainPageModuleOutput: class {}
+
+// MARK: - SceneProtocol
+
+/// methods for communication Presenter -> View
+protocol MainPageViewInput: class {}
+
+/// methods for communication View -> Presenter
+protocol MainPageViewOutput {}
+
+/// methods for communication Presenter -> Interactor
+protocol MainPageInteractorInput {}
+
+/// methods for communication Interactor -> Presenter
+protocol MainPageInteractorOutput: class {}
